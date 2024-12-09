f032088ac91e6f7c2eed08ab43da73dbcdaf3d63
ZhukovaEkaterinaB21-215 <116277216+ZhukovaEkaterinaB21-215@users.noreply.github.com>
2024-12-09 10:00:39 +0300

    Update task2.h

--- cpp/task2.h
+++ cpp/task2.h
@@ -1,5 +1,5 @@
 #pragma once
 
 int N(Point p0, Point p1, Point p2, Point p3);
-Point B_splain(Point p0, Point p1, Point p2, Point p3, double t);
+Point B_spline(Point p0, Point p1, Point p2, Point p3, double t);
 void Curve(Mat& image, vector <Point>& vertex, string color);
